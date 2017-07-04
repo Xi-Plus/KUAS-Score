5e218f6efb2b482d215867c5d7124c8e2fffa2d3
xiplus <huangxuanyuxiplus@gmail.com>
2017-07-04 09:19:10 +0800

    overall score

--- index.php
+++ index.php
@@ -24,24 +24,49 @@ if ($res === false) {
 	exit("fetch fail\n");
 }
 $res = html_entity_decode($res);
-preg_match_all("/<td align=left>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)/", $res, $m);
 
 $data = file_get_contents($C["datapath"]);
 $data = json_decode($data, true);
 $message = "";
-foreach ($m[0] as $key => $value) {
-	$class = trim($m[1][$key]);
-	$score = (int)(trim($m[7][$key]));
-	if (!isset($data[$class])) {
-		$data[$class] = $score;
-	} else if ($data[$class] != $score) {
-		echo $class." update to ".$score."\n";
-		$data[$class] = $score;
-		$message .= $class." 的成績已公布\n";
-	} else {
-		echo $class." is ".$score."\n";
+
+if (preg_match_all("/<td align=left>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)<\/td><td>([^<]+)/", $res, $m)) {
+	foreach ($m[0] as $key => $value) {
+		$class = trim($m[1][$key]);
+		$score = (int)(trim($m[7][$key]));
+		if (!isset($data[$class])) {
+			$data[$class] = $score;
+		} else if ($data[$class] != $score) {
+			echo $class." update to ".$score."\n";
+			$data[$class] = $score;
+			$message .= $class." 的成績已公布\n";
+		} else {
+			echo $class." is ".$score."\n";
+		}
 	}
+} else {
+	echo "match fail 1\n";
 }
+
+if (preg_match("/操行成績：(.*?)　　　　總平均：(.*?)　　　　班名次\/班人數：(.*?)\/.*?　　　　班名次百分比：(.*?)%/", $res, $m)) {
+	$name = ["", "操行成績", "總平均", "班名次", "班名次百分比"];
+	for ($i=1; $i <= 4; $i++) {
+		$class = $name[$i];
+		$score = $m[$i];
+		if (!isset($data[$class])) {
+			$data[$class] = $score;
+		} else if ($data[$class] != $score) {
+			echo $class." update to ".$score."\n";
+			$data[$class] = $score;
+			$message .= $class." 已公布\n";
+		} else {
+			echo $class." is ".$score."\n";
+		}
+	}
+} else {
+	echo "match fail 2\n";
+}
+
+file_put_contents($C["datapath"], json_encode($data, 256));
 
 if ($message != "") {
 	$post = array(
@@ -49,5 +74,4 @@ if ($message != "") {
 		"access_token" => $C['FBtoken']
 	);
 	$res = cURL($C['FBAPI']."me/feed", $post);
-	file_put_contents($C["datapath"], json_encode($data, 256));
 }
